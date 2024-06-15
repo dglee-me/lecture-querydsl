e6217b3f1a4b9885fd141943b960b8c44915226c
dglee-me <dglee.dev@gmail.com>
2024-06-15 10:59:55 +0900

    init

---
+++ src/main/java/kr/co/dglee/lecture/LectureQuerydslApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureQuerydslApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LectureQuerydslApplication.class, args);
+  }
+
+}
